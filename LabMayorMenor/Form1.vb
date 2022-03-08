1ba9b76d99e9ac83b44a5c8f707551feda4aed7c
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 08:52:30 -0600

    Agregar archivos de proyecto.

---
+++ LabMayorMenor/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer = TextBox1.Text
+        Dim d As Integer = TextBox2.Text
+        Dim c As Integer = TextBox3.Text
+        Dim b As Integer
+        If a > d And a > c Then
+            b = a
+        ElseIf d > a And d > c Then
+            b = d
+        ElseIf c > a And c > d Then
+            b = c
+        End If
+        TextBox4.Text = b
+
+    End Sub
+End Class
